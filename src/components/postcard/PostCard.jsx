251fb013f2e458273ae35c3c1b591a4df4b6c7ba
mulfranck <njifranclin@gmail.com>
2023-06-07 12:04:58 +0100

    restrusture blog post layout

--- src/components/postcard/PostCard.jsx
+++ src/components/postcard/PostCard.jsx
@@ -2,22 +2,50 @@ import { Link } from 'react-router-dom';
 import './PostCard.css';
 
 
-const PostCard = ({blog}) => {
+const PostCard = (props) => {
+    const { title, tags, body } = props.article;
 
     let blogSlug = 'nigga'
 
+    console.log("post card props")
+    console.log(props.article.userId)
+
     return (
-        <Link to={`/blog/${blogSlug}`} className="card bg-dark text-decoration-none text-white m-4" id='post-card'>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <Link to={`/blog/${blogSlug}`} className="card m-4 text-decoration-none text-dark row" id='post-card'>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="..." className="img-fluid rounded-start" alt="..." />
                 </div>
-                {/* <div className="card-body">
-                    < href="/" className="card-link">Card link</>
-                    <a href="/" className="card-link">Another link</a>
-                </div> */}
+                <div className="col-md-8">
+                    <div className="card-body text-start">
+                        <h5 className="card-title flex-start text-start">{title}</h5>
+                        <p className="card-text text__text--fixwidth">{body}</p>
+                        <div className="p-0 d-flex align-items-center text-muted justify-content-between">
+                            <p className="card-text m-0">
+                                <small className="date-published"> date | </small>
+                                <small className="text-muted">Last updated 3 mins ago</small> |
+                                <small className="catergory"> { tags[0] }</small>
+                            </p>
+                            <p className="cart-text fs-5 m-0 d-flex align-items-center">
+                                {/* <button type="button" class="btn btn-primary position-relative"> */}
+                                <small class="bi bi-hand-thumbs-up small mx-1"></small>
+                                    {/* <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                        {reactions}
+                                        <span class="visually-hidden">unread messages</span>
+                                    </span> */}
+                                {/* </button> */} |
+                                <small className="bookmark ms-1">
+                                    <i class="bi bi-bookmark-plus"></i>
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </Link>
+
 
     )
 }
